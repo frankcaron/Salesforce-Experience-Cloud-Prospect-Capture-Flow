af5f63f15f1bb36265c42537a3776c7b479d13a6
Frank Caron <frank@frankcaron.com>
2020-11-26 08:07:10 -0500

    Built out initial flow

--- force-app/main/default/lwc/excRegistrationFlow/excRegistrationFlow.js
+++ force-app/main/default/lwc/excRegistrationFlow/excRegistrationFlow.js
@@ -1,3 +1,42 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class ExcRegistrationFlow extends LightningElement {}
+export default class ExcRegistrationFlow extends LightningElement {
+
+    //Properties
+    @api nbaStrategy;
+
+    //Step Flags
+    stepOne = true;
+    stepTwo = false;
+    stepThree = false;
+
+    //Form Inputs
+    name;
+
+    //Helper Functions
+    progressStep() {
+        if (this.stepOne) {
+
+            // Progress the step
+            this.stepOne = false;
+            this.stepTwo = true;
+
+        } else if (this.stepTwo) {
+
+            //Progress the step
+            this.stepTwo = false;
+            this.stepThree = true;
+        }
+    }
+
+    progressStepEnterKey(event) {
+        if (event.which == 13){
+            this.progressStep();
+        }
+    }
+
+    grabName(event) {
+        this.name = event.target.value;
+    }
+
+}
